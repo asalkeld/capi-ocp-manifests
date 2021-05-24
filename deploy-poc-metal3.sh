0032c895e92fb5f27ffbc4873e1adf447f18cdeb
Angus Salkeld <asalkeld@redhat.com>
2021-05-24 15:44:10 +1000

    set WORKER_MACHINE_COUNT

--- deploy-poc-metal3.sh
+++ deploy-poc-metal3.sh
@@ -2,13 +2,6 @@
 
 source deploy-poc-common.sh
 
-# consts
-export NAMESPACE="ocp-cluster-api"
-export API_ENDPOINT_PORT="6443"
-export IMAGE_CHECKSUM_TYPE="sha256"
-export IMAGE_FORMAT="qcow2"
-export NODE_DRAIN_TIMEOUT="0s"
-
 # TODO(Angus) replace with service-ca
 echo -e "${GREEN}Install Cert Manager (TEMP)${NOCOLOR}"
 oc apply -f operator/cert-manager.yaml
@@ -37,6 +30,12 @@ oc apply -f operator/operator.yaml
 
 echo -e "${GREEN}Create CAPI resources: CAPIDeployment, MachineSet, Metal3MachineTemplate${NOCOLOR}"
 
+export NAMESPACE="ocp-cluster-api"
+export API_ENDPOINT_PORT="6443"
+export IMAGE_CHECKSUM_TYPE="sha256"
+export IMAGE_FORMAT="qcow2"
+export NODE_DRAIN_TIMEOUT="0s"
+
 export API_ENDPOINT_HOST=$(oc get infrastructure/cluster -o json | jq ".status.platformStatus.baremetal.apiServerInternalIP")
 
 pushd ${SCRIPTDIR}
@@ -53,5 +52,6 @@ export POD_CIDR=${CLUSTER_SUBNET_V4}
 export SERVICE_CIDR=${SERVICE_SUBNET_V4}
 export IMAGE_URL=http://$MIRROR_IP/images/${MACHINE_OS_IMAGE_NAME}
 export IMAGE_CHECKSUM=http://$MIRROR_IP/images/${MACHINE_OS_IMAGE_NAME}.sha256sum
+export WORKER_MACHINE_COUNT=$NUM_EXTRA_WORKERS
 
 envsubst < examples/capm3-resources.yaml | oc apply -f -
